61bba4c5f9ea77aab6ffa7bc003db739367f6e6b
Neeraj1850 <neerajreddy1850@gmail.com>
2024-06-18 08:46:29 -0600

    integrated api call

--- app/(tabs)/Home/Result.js
+++ app/(tabs)/Home/Result.js
@@ -1,28 +1,82 @@
-import React from 'react';
-import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, SafeAreaView, TouchableOpacity, ActivityIndicator } from 'react-native';
 import styles from '../../styles/result.style'
 import mainStyle from '../../styles/general.style';
-import { router } from 'expo-router';
+import { router, useLocalSearchParams } from 'expo-router';
+import axios from 'axios';
 
 const ResultInfo = () => {
+
+    const {appliance, price} = useLocalSearchParams()
+
+    console.log(appliance, price)
+
+    //const [loading, setLoading] = useState(true);
+    //const [applianceData, setApplianceData] = useState(null);
+    //const { uri, name, type } = useLocalSearchParams();
+
+    // async function fetchData() {
+
+    //     const formData = new FormData();
+    //     formData.append('appliance_photo', {
+    //         uri: "file:///data/user/0/host.exp.exponent/cache/ExperienceData/%40anonymous%2Fapp-cafa0c31-fada-4505-80f8-7af3d8daf5cf/ImagePicker/f62aa7cc-9b55-4801-bd73-1684f9f64ecd.webp",
+    //         name: 'upload.webp',
+    //         type: 'image/webp'
+    //     });
+        
+    //     try {
+    //         console.log('Sending FormData', formData._parts[0]);
+    //         const response = await fetch('http://10.0.0.176:8000/upload/', {
+    //             method: 'POST',
+    //             body: formData,
+    //             headers: {
+    //               'Accept': 'application/json',
+    //             },
+    //         })
+    //         console.log(response)
+    //         if (!response.ok) {
+    //             throw new Error(`HTTP status ${response.status}`);
+    //         }
+    //         const responseData = await response.json();
+    //         console.log('responseData',responseData)
+    //         console.log('Fetch successful:', responseData);
+    //     } catch (error) {
+    //         console.error('Fetch error:', error);
+    //         alert('Failed to fetch data');
+    //     }
+    // }
+
+    
+    // fetchData();
+
+    // if (loading) {
+    //     return (
+    //         <ActivityIndicator style={{
+    //             flex: 1,
+    //             justifyContent: 'center',
+    //             alignItems: 'center',
+    //             backgroundColor: '#4F9C80'
+    //         }} size="large" color="#D9D9D9"/>
+    //     );
+    // }
     return (
         <SafeAreaView style={mainStyle.container}>
             <View style={styles.container}>
                 <View style={styles.infoRow}>
                     <Text style={styles.label}>Appliance:</Text>
-                    <Text style={styles.value}>Refrigerator</Text>
+                    <Text style={styles.value}>{appliance}</Text>
                 </View>
                 <View style={styles.infoRow}>
                     <Text style={styles.label}>Brand:</Text>
-                    <Text style={styles.value}>Samsung</Text>
+                    <Text style={styles.value}>{NaN}</Text>
                 </View>
                 <View style={styles.infoRow}>
                     <Text style={styles.label}>Type:</Text>
-                    <Text style={styles.value}>Bottom Freezer</Text>
+                    <Text style={styles.value}>{NaN}</Text>
                 </View>
                 <View style={styles.infoRow}>
                     <Text style={styles.label}>Power{'\n'}Consumption:</Text>
-                    <Text style={styles.value}>$100/Month</Text>
+                    <Text style={styles.value}>{price}</Text>
                 </View>
             </View>
             <TouchableOpacity style={mainStyle.button}>
--- app/(tabs)/Home/index.js
+++ app/(tabs)/Home/index.js
@@ -1,23 +1,114 @@
 import React from "react";
-import { Text, TouchableOpacity, SafeAreaView } from "react-native";
-import { router } from "expo-router";
-import UploadCard from "../../components/UploadCard";
+import { Text, TouchableOpacity, View, SafeAreaView } from "react-native";
+import { Svg, Path } from "react-native-svg";
+import styles from "../../styles/uploadCard.style";
 import mainStyle from "../../styles/general.style";
-import { Link, useNavigation } from 'expo-router';
+import * as ImagePicker from 'expo-image-picker';
+import { useState, useEffect } from "react";
+import { router } from "expo-router";
 
 const Home = () => {
-  const navigation = useNavigation();
-    return (
-    <SafeAreaView style={mainStyle.container}>
-          <UploadCard />
-          <TouchableOpacity style={mainStyle.button} onPress={() => {
-            router.navigate('Home/Result')
-          }}>
-            <Text style={mainStyle.text}>Calculate</Text>
-          </TouchableOpacity>
-        
-    </SafeAreaView>
-    );
+
+  const [image, setImage] = useState()
+  const [info, setInfo] = useState()
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      quality: 1,
+    });
+
+    if (!result.canceled && result.assets) {
+      const image = result.assets[0];
+      setImage(image)
+      const formData = new FormData();
+      formData.append('appliance_photo', {
+        uri: image.uri,
+        name: image.fileName || 'upload.webp',
+        type: 'image/webp'
+      })
+
+      try {
+        const response = await fetch('http://10.0.0.176:8000/upload/', {
+          method: 'POST',
+          body: formData,
+          headers: {
+            'Accept': 'application/json',
+          },
+        });
+      
+        if (!response.ok) {
+          throw new Error(`HTTP status ${response.status}`);
+        }
+  
+        const data = await response.json();
+        setInfo(data)
+        console.log(data);
+        alert('Success', data);
+  
+      } catch (error) {
+        console.error('Error uploading image:', error);
+        alert('ERROR', error.toString());
+      }
+    }
+    
+  }
+
+
+  return (
+  <SafeAreaView style={mainStyle.container}>
+        <View style={styles.container}>
+            <View style={styles.miniContainer}>
+                <View style={styles.uploadContainerBox}>
+                    <View style={styles.uploadContainer}>
+                        <Text style={styles.text}>Appliance Upload</Text>
+                    </View>
+                    <TouchableOpacity >
+                        <Svg width="5" height="10" viewBox="0 0 5 10" fill="none" xmlns="http://www.w3.org/2000/svg" style={styles.miniIcon}>
+                            <Path d="M1.25 1C1.25 0.734784 1.3817 0.48043 1.61612 0.292893C1.85054 0.105357 2.16848 0 2.5 0C2.83152 0 3.14946 0.105357 3.38388 0.292893C3.6183 0.48043 3.75 0.734784 3.75 1C3.75 1.26522 3.6183 1.51957 3.38388 1.70711C3.14946 1.89464 2.83152 2 2.5 2C2.16848 2 1.85054 1.89464 1.61612 1.70711C1.3817 1.51957 1.25 1.26522 1.25 1ZM0 4C0 3.63125 0.372396 3.33333 0.833333 3.33333H2.5C2.96094 3.33333 3.33333 3.63125 3.33333 4V8.66667H4.16667C4.6276 8.66667 5 8.96458 5 9.33333C5 9.70208 4.6276 10 4.16667 10H0.833333C0.372396 10 0 9.70208 0 9.33333C0 8.96458 0.372396 8.66667 0.833333 8.66667H1.66667V4.66667H0.833333C0.372396 4.66667 0 4.36875 0 4Z" fill="white"/>
+                        </Svg>
+                    </TouchableOpacity>
+                </View>
+                <TouchableOpacity style={mainStyle.button} onPress={() => pickImage()}>
+                    <Text style={mainStyle.text}>Upload</Text>
+                </TouchableOpacity>     
+            </View>
+            <View style={styles.miniContainer}>
+                <View style={styles.uploadContainerBox}>
+                    <View style={styles.uploadContainer}>
+                        <Text style={styles.text}>Energy Sticker Upload (optional)</Text>
+                    </View>
+                    <TouchableOpacity>
+                        <Svg width="5" height="10" viewBox="0 0 5 10" fill="none" xmlns="http://www.w3.org/2000/svg" style={styles.miniIcon}>
+                            <Path d="M1.25 1C1.25 0.734784 1.3817 0.48043 1.61612 0.292893C1.85054 0.105357 2.16848 0 2.5 0C2.83152 0 3.14946 0.105357 3.38388 0.292893C3.6183 0.48043 3.75 0.734784 3.75 1C3.75 1.26522 3.6183 1.51957 3.38388 1.70711C3.14946 1.89464 2.83152 2 2.5 2C2.16848 2 1.85054 1.89464 1.61612 1.70711C1.3817 1.51957 1.25 1.26522 1.25 1ZM0 4C0 3.63125 0.372396 3.33333 0.833333 3.33333H2.5C2.96094 3.33333 3.33333 3.63125 3.33333 4V8.66667H4.16667C4.6276 8.66667 5 8.96458 5 9.33333C5 9.70208 4.6276 10 4.16667 10H0.833333C0.372396 10 0 9.70208 0 9.33333C0 8.96458 0.372396 8.66667 0.833333 8.66667H1.66667V4.66667H0.833333C0.372396 4.66667 0 4.36875 0 4Z" fill="white"/>
+                        </Svg>
+                    </TouchableOpacity>
+                </View>
+                <TouchableOpacity style={mainStyle.button} onPress={() => pickImage()}>
+                    <Text style={mainStyle.text}>Upload</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+        <TouchableOpacity 
+          style={mainStyle.button}
+          onPress={() => {
+            if (image) {
+              router.navigate({
+                pathname: 'Home/Result',
+                params: {
+                  appliance: info.predictedClass,
+                  price: info.totalCost
+                }
+              });
+            } else {
+              alert('Please select an image first.');
+            }
+        }}>
+          <Text style={mainStyle.text}>Calculate</Text>
+        </TouchableOpacity>
+      
+  </SafeAreaView>
+  );
 
 }
 
